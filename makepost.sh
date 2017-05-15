e6dde6f96b2e39047a3360a76dcc0f9a9e73d73b
shamison <unknown0incident@gmail.com>
2017-05-15 20:08:25 +0900

    update for eazy to post

--- makepost.sh
+++ makepost.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 makepost() {
     TITLE="diary"
@@ -6,14 +6,36 @@ makepost() {
         TITLE=$1
     fi
     DATE=`date '+%Y-%m-%d'`
-    POSTPATH="_posts/${DATE}-${TITLE}.md"
-    if [ -e ${POSTPATH} ]; then
-        emacs -nw ${POSTPATH}
-    else
-        echo "---\nlayout: post\ntitle: たいとる\n---\n" > ${POSTPATH}
-        emacs -nw ${POSTPATH}
+    # 既に${DATE}-${TITLE}.mdが存在するなら
+    if [ -e "_posts/${DATE}-${TITLE}.md" ]; then       
+        COUNTS=`ls -U1  _posts/ | grep ${DATE}-${TITLE} | wc -l | tr -d ' '`
+        # ${DATE}-${TITLE}-${同名のファイル数}.mdを作るか聞く
+        printf "\e[32m#\e[m Same md files already exits\n"
+        printf "\e[32m#\e[m Do you wanna make new file, "
+        printf "\e[31m${TITLE}-${COUNTS}.md\e[m ? (y/n):"
+        read ans
+        case $ans in
+            # yes ならば 新しいファイルを作る
+            #     file名は diary.md, diary-1.md, diary-2.md, ... と値が増える 
+            [Yy] | [Yy][Ee][Ss] )
+            ;;
+            # no ならば COUNTSを減らして最新のファイルを指すようにする
+            * )
+                COUNTS=$((COUNTS - 1));; 
+        esac
+        if [ ${COUNTS} -ne 0 ]; then
+            TITLE=${TITLE}-${COUNTS}
+        fi
     fi
-    echo ${POSTPATH}
+    POSTPATH="_posts/${DATE}-${TITLE}.md"
+    # もしファイルがないときは作成
+    if [ ! -f ${POSTPATH} ]; then
+        echo -e "---\nlayout: post\ntitle: たいとる\n---\n" > ${POSTPATH}        
+    fi
+    # 最新のファイルを開く
+    emacs -nw ${POSTPATH}
+    printf "\e[32m#\e[m opened, ${POSTPATH}\n"
 }
 
 makepost $1
+
